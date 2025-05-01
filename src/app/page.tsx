04eb8fc50b0e0b239fba1071d7d59672fb3901b1
raymondyangdev <raymond16@hotmail.co.nz>
2025-05-02 10:55:15 +1200

    feat: add Hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Hero from '@/components/Hero';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className="flex flex-1 items-center justify-center w-full">
+      <Hero />
+    </main>
+  );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import IconButton from '@mui/material/IconButton';
+import { FileTextIcon } from '@radix-ui/react-icons';
+import { Button } from '@/components/ui/button';
+
+export default function Hero() {
+  return (
+    <section
+      id="hero"
+      className="flex flex-col w-4/5 md:w-1/2 min-h-[35vh] md:min-h-[60vh] mt-24 md:mt-0 gap-4 md:gap-5">
+      <div>
+        <h1 className="text-4xl md:text-5xl font-semibold mb-4">Hey, I&apos;m Raymond 👋</h1>
+        <h3 className="text-xl md:text-3xl mb-2">Build. Learn. Innovate.</h3>
+        <div className="flex flex-col md:flex-row md:gap-10">
+          <h4 className="text-sm md:text-base font-semibold mb-4">📍 Auckland, New Zealand</h4>
+          <h4 className="text-sm md:text-base font-semibold">📚 CS @ University of Auckland</h4>
+        </div>
+      </div>
+
+      <div className="flex w-1/2 md:w-1/6 justify-between">
+        <IconButton aria-label="GitHub.com" onClick={() => window.open('https://github.com/raymondyangdev')}>
+          <GitHubIcon className="icon" />
+        </IconButton>
+
+        <IconButton
+          aria-label="LinkedIn.com"
+          onClick={() => window.open('https://www.linkedin.com/in/raymondyangdev/')}>
+          <LinkedInIcon className="icon" />
+        </IconButton>
+
+        <IconButton href="mailto:raymondyangnz1+contact@gmail.com">
+          <EmailIcon className="icon" />
+        </IconButton>
+      </div>
+
+      <div>
+        <Button
+          className="w-32 md:w-36 mt-2 md:mt-6"
+          onClick={() => window.open('./assets/misc/raymond-yang-cv-2025.pdf', '_blank')}>
+          <FileTextIcon className="mr-2 h-4 w-4" />
+          Resume
+        </Button>
+      </div>
+    </section>
+  );
+}
