cb77675ae1024e8d2c1e8fa74d2e2602de936d4a
raymondyangdev <raymond16@hotmail.co.nz>
2025-05-02 11:03:07 +1200

    fix: update Blog link to use fragment identifier

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,13 +7,12 @@ export default function Navbar() {
   return (
     <nav
       className={`z-10 h-[12vh] w-[85vw] md:w-[50vw] md:max-w-[50vw] flex justify-center items-center absolute md:absolute left-1/2 top-0 transform -translate-x-1/2`}>
-      <div className="flex w-full justify-between items-center">
-        {/* DESKTOP */}
+      <div className="flex w-full items-center md:justify-end">
         <div className="flex justify-end gap-10 items-center">
           <div className="hover:cursor-pointer underline-animation" onClick={() => router.push('/')}>
             Home
           </div>
-          <div className="hover:cursor-pointer underline-animation" onClick={() => router.push('/blog')}>
+          <div className="hover:cursor-pointer underline-animation" onClick={() => router.push('#blog')}>
             Blog
           </div>
         </div>
