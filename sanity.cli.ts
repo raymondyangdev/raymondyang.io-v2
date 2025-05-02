2efc1a3e669a51a77a8f4aa1d5fb7b6990b8b27b
raymondyangdev <raymond16@hotmail.co.nz>
2025-05-02 12:09:25 +1200

    fix: fix Sanity environment configuration

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -4,7 +4,8 @@
 **/
 import { defineCliConfig } from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID;
+const dataset = process.env.SANITY_STUDIO_DATASET;
+const studioHost = process.env.STUDIO_HOST;
 
-export default defineCliConfig({ api: { projectId, dataset } })
+export default defineCliConfig({ api: { projectId, dataset }, studioHost });
