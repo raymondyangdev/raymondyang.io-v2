0c0e4ea45dc120d82f5e3fb668267510f4833561
raymondyangdev <raymond16@hotmail.co.nz>
2025-05-13 17:34:14 +1200

    feat: init sanity

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { postType } from './postType';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+  types: [postType],
+};
