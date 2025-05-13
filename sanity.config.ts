633a7fd45798cdfd00fbcdd8e635d7d00f083944
raymondyangdev <raymond16@hotmail.co.nz>
2025-05-13 20:26:13 +1200

    refactor: format imports and update schema structure in sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -10,19 +10,21 @@ import {structureTool} from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from './env';
-import {schema} from './src/sanity/schemaTypes'
-import {structure} from './src/sanity/structure'
+import { schema } from './src/sanity/schemaTypes';
+import { structure } from './src/sanity/structure';
 
 export default defineConfig({
   basePath: '/admin',
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
-  schema,
+  schema: {
+    types: schema.types,
+  },
   plugins: [
-    structureTool({structure}),
+    structureTool({ structure }),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    visionTool({ defaultApiVersion: apiVersion }),
   ],
-})
+});
