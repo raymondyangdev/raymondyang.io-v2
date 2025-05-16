8f4cecfb1bce0de403b9e0fcda5a713e99c5f931
raymondyangdev <raymond16@hotmail.co.nz>
2025-05-16 12:04:18 +1200

    feat: add Projects component

---
+++ src/components/projects/Projects.tsx
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <section className="flex flex-col gap-4">
+      <h2 className="text-4xl">projects.</h2>
+      <p className="text-xl">Coming soon...</p>
+    </section>
+  );
+}
