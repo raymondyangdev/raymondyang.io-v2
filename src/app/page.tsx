3a04402cfd3c14e79fb94f6798e8136644a602ad
raymondyangdev <raymond16@hotmail.co.nz>
2025-05-22 10:03:27 +1200

    feat: fetch and pass projects data to Projects component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,15 +12,29 @@ const BLOGS_QUERY = `*[
   _type == "post"
   && defined(slug.current)
 ]|order(publishedAt desc)[0...2]{_id, title, slug, publishedAt, body}`;
+
+const PROJECTS_QUERY = `*[
+  _type == "project"
+]|order(_createdAt desc)[0...3]{
+  _id,
+  title,
+  description,
+  tags,
+  image,
+  github,
+  live
+}`;
+
 const options = { next: { revalidate: 30 } };
 
 export default async function Home() {
   const cvFile = await client.fetch<{ url: string }>(PDF_QUERY, {}, options);
   const recentPosts = await client.fetch(BLOGS_QUERY, {}, options);
+  const projects = await client.fetch(PROJECTS_QUERY, {}, options);
   return (
     <main className="flex flex-col gap-8 py-4">
       <Hero cvUrl={cvFile.url} />
-      <Projects />
+      <Projects projects={projects} />
       <Blog recentPosts={recentPosts} />
     </main>
   );
